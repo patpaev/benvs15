e729748cf7d27f03741b84eed84aa5a825f327a2
Patrick P. Paevere <patrick@paevere.com>
2015-08-19 11:57:12 +1000

    update nexus forms, remove commented-out code

--- sites/all/themes/msd15/templates/region--footer.tpl.php
+++ sites/all/themes/msd15/templates/region--footer.tpl.php
@@ -1,37 +1,9 @@
-<footer class="cta hidden">
-  <ul>
-    <li>
-      <a href="#">
-        <svg role="img" class="icon">
-          <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-facebook"></use>
-        </svg><!--
-        -->Facebook
-      </a>
-    </li>
-    <li>
-      <a href="#">
-        <svg role="img" class="icon">
-          <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-twitter"></use>
-        </svg><!--
-        -->Twitter
-      </a>
-    </li>
-    <li>
-      <a href="#">
-        <svg role="img" class="icon">
-          <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-linkedin"></use>
-        </svg><!--
-        -->LinkedIn
-      </a>
-    </li>
-  </ul>
-</footer>
 <div class="enquiry">
   <div class="enquiry__cta accordion__title">
     Have a question? <a class="button-small" href="enquiry-form">Enquire now</a>
   </div>
   <div class="enquiry__form accordion__hidden">
-    <iframe frameborder="0" src="https://enquiry.app.unimelb.edu.au/?formtype=ugraduate1&amp;cc=B-ENVS&amp;fn=OA" height="1419" width="700" style="display: inherit; margin: 0 auto; max-width: 700px; width: 100%; "></iframe>
+    <iframe frameborder="0" src="https://enquiry.app.unimelb.edu.au/Web3/?formtype=ugraduate1&amp;cc=B-ENVS&amp;fn=OA" height="3200" width="700" style="display: inherit; margin: 0 auto; max-width: 700px; width: 100%; "></iframe>
   </div>
   <div class="enquiry__subline">
     <div class="first">
